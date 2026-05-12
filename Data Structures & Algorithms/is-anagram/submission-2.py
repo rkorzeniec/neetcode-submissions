be0cb4349dc3798644e2a1cb15ee7c576478bfe6
Robert Korzeniec <r.korzeniec@gmail.com>
2026-05-12 09:22:47 +0200

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,33 @@
+class Solution:
+    def _build_cache(self, s: str) -> dict[str]:
+        cache = {}
+
+        for char in s:
+            if char in cache:
+                cache[char] += 1
+            else:
+                cache[char] = 1
+        
+        return cache
+
+    def isAnagram(self, s: str, t: str) -> bool:
+        cache_s = self._build_cache(s)
+        cache_t = self._build_cache(t)
+        
+        for char in cache_s.keys():
+            if char not in cache_t:
+                return False
+
+            if cache_s[char] != cache_t[char]:
+                return False
+
+        for char in cache_t.keys():
+            if char not in cache_s:
+                return False
+
+            if cache_t[char] != cache_s[char]:
+                return False
+
+        return True
+
+        
